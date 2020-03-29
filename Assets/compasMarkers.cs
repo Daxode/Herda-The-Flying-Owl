127e425a2314efb165fb429294c935090751a02b
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2020-03-29 04:52:06 +0200

    JumpTicking

--- Assets/compasMarkers.cs
+++ Assets/compasMarkers.cs
@@ -15,10 +15,10 @@ public class CompasMarkers : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (MovementFlying.Instance != null) {
+        if (Flying.Instance != null) {
             Vector2 idk = rectT.anchoredPosition;
-            Vector3 vecPlayerForward = Vector3.Scale(MovementFlying.Instance.transform.forward, new Vector3(1, 0, 1));
-            Vector3 vecPlayerRight = Vector3.Scale(MovementFlying.Instance.transform.right, new Vector3(1, 0, 1));
+            Vector3 vecPlayerForward = Vector3.Scale(Flying.Instance.transform.forward, new Vector3(1, 0, 1));
+            Vector3 vecPlayerRight = Vector3.Scale(Flying.Instance.transform.right, new Vector3(1, 0, 1));
             Vector3 vecDirToTarget = Quaternion.AngleAxis(rotAngle, Vector3.forward)*new Vector3(0, 0, 1);
 
             float angle = Mathf.Deg2Rad * Vector3.Angle(vecDirToTarget, vecPlayerForward);
--- Assets/compasTarget.cs
+++ Assets/compasTarget.cs
@@ -19,11 +19,11 @@ public class CompasTarget : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (target != null && MovementFlying.Instance != null) {
+        if (target != null && Flying.Instance != null) {
             Vector2 idk = rectT.anchoredPosition;
-            Vector3 vecPlayerForward = Vector3.Scale(MovementFlying.Instance.transform.forward, new Vector3(1, 0, 1));
-            Vector3 vecPlayerRight = Vector3.Scale(MovementFlying.Instance.transform.right, new Vector3(1, 0, 1));
-            Vector3 vecDirToTarget = Vector3.Scale(MovementFlying.Instance.transform.position - target.position, new Vector3(1, 0, 1));
+            Vector3 vecPlayerForward = Vector3.Scale(Flying.Instance.transform.forward, new Vector3(1, 0, 1));
+            Vector3 vecPlayerRight = Vector3.Scale(Flying.Instance.transform.right, new Vector3(1, 0, 1));
+            Vector3 vecDirToTarget = Vector3.Scale(Flying.Instance.transform.position - target.position, new Vector3(1, 0, 1));
 
             float angle = Mathf.Deg2Rad * Vector3.Angle(vecDirToTarget, vecPlayerForward);
             if (Vector3.Dot(vecDirToTarget, vecPlayerRight) > 0) angle += Mathf.PI;
