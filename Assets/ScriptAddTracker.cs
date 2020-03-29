3a673fcf4ce7b27fd055cf10a97ec12c4f586cbd
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2020-03-29 01:12:10 +0100

    Peasents jumping

---
+++ Assets/ScriptAddTracker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAddTracker : MonoBehaviour {
+    public CompasMarkers compasMarkers;
+    public Sprite showThis;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
