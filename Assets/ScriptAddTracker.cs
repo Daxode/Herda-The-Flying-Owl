3d8b99637f4f7135a80af9f50e475f7eba30988e
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2020-03-29 01:39:54 +0100

    Working compass

--- Assets/ScriptAddTracker.cs
+++ Assets/ScriptAddTracker.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScriptAddTracker : MonoBehaviour {
-    public CompasMarkers compasMarkers;
+    public GameObject compasTarget;
     public Sprite showThis;
     // Start is called before the first frame update
     void Start() {
-        
+        GameObject ct = Instantiate(compasTarget, GameObject.FindWithTag("MarkerViewer").transform);
+        Image im = ct.GetComponent<Image>();
+
+        im.sprite = showThis;
+        ct.GetComponent<CompasTarget>().SetTarget(transform);
     }
 
     // Update is called once per frame
